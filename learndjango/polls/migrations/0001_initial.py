60ce9632f56b46d87d44d35e7acf974201d82941
Ahmad Soliman <ahmadadel92@gmail.com>
2015-10-26 00:24:00 +0200

    added polls app with model

---
+++ learndjango/polls/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Choise',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('choice', models.CharField(max_length=200)),
+                ('votes', models.IntegerField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Poll',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('question', models.CharField(max_length=200)),
+                ('pub_date', models.DateTimeField(verbose_name=b'date published')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='choise',
+            name='poll',
+            field=models.ForeignKey(to='polls.Poll'),
+        ),
+    ]
